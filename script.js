d17f966a9ce57b532b42b0ad39e038c75e9a2bb6
Padurean-Robert <robimondialu1200@yahoo.com>
2024-10-19 23:31:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,27 @@
 let dictionary = [];
 
-document.getElementById('addWord').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent form submission from reloading the page
-    const word = document.getElementById('add-input').value.trim();
-
+function addWord {
+    event.preventDefault(); 
+    const word = document.getElementById('add-input').value.trim(); 
     if (word) {
         dictionary.push(word);
         document.getElementById('result').innerHTML = `<span class="text-success">"${word}" added to the dictionary!</span>`;
     }
     document.getElementById('add-input').value = ''; 
-})
+}
 
-document.getElementById('searchWord').addEventListener('submit', function (event) {
-    event.preventDefault(); 
-    const searchWord = document.getElementById('search-input').value.trim(); 
-
+function searchWordV {
     if (searchWord) {
-        if (dictionary.includes(searchWord)) {
-            document.getElementById('result').innerHTML = `<span class="text-success">"${searchWord}" is in the dictionary!</span>`;
+        event.preventDefault();
+        const searchWord = document.getElementById('search-imput').value.trim();
+        if (searchWord) {
+            if (dictionary.includes(searchWord)) {
+                document.getElementById('result').innerHTML = `<span class="text-success">"${searchWord} is in the dictionary!</span`;
+            } else {
+                document.getElementById('result').innerHTML = `<span class="text-danger">"${searchWord}" is not in the dictionary.</span>`;
+            }
         } else {
-            document.getElementById('result').innerHTML = `<span class="text-danger">"${searchWord}" is not in the dictionary.</span>`;
+            document.getElementById('result').innerHTML = `<span class="text-danger">Please enter a word to search.</span>`;
         }
-    } else {
-        document.getElementById('result').innerHTML = `<span class="text-danger">Please enter a word to search.</span>`;
-    }
-    document.getElementById('search-input').value = ''; 
-});
+    document.getElementById('search-input').value = '';
+}
