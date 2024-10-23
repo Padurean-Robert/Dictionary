3562ba875cfc7b9a16b6fb7e3bed30a7ec345270
Padurean-Robert <robimondialu1200@yahoo.com>
2024-10-23 12:20:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
 let dictionary = [];
 
-function addWord(event) {
-    event.preventDefault(); 
-    const word = document.getElementById('add-input').value.trim(); 
+function addWord() {
+    const word = document.getElementById('word-input').value.trim(); 
     if (word) {
-        dictionary.push(word);
-        document.getElementById('result').innerHTML = 
-        `<span class="text-success">"${word}" added to the dictionary!</span>`;
+        if (!dictionary.includes(word)) {
+            dictionary.push(word);
+            document.getElementById('result').innerHTML = `<span class="text-success">"${word}" added to the dictionary!</span>`;
+        } else {
+            document.getElementById('result').innerHTML = `<span class="text-warning">"${word}" is already in the dictionary.</span>`;
+        }
+    } else {
+        document.getElementById('result').innerHTML = `<span class="text-danger">Please enter a word to add.</span>`;
     }
-    document.getElementById('add-input').value = ''; 
+    document.getElementById('word-input').value = '';
 }
 
-function searchWord(event) {
-        event.preventDefault();
-        const searchWord = document.getElementById('search-input').value.trim();
-        if (searchWord) {
-            if (dictionary.includes(searchWord)) {
-                document.getElementById('result').innerHTML = 
-                `<span class="text-success">"${searchWord} is in the dictionary!</span`;
-            } else {
-                document.getElementById('result').innerHTML = 
-                `<span class="text-danger">"${searchWord}" is not in the dictionary.</span>`;
-            }
-        } else {
-            document.getElementById('result').innerHTML = 
-            `<span class="text-danger">Please enter a word to search.</span>`;
-        }
-        document.getElementById('search-input').value = '';
+function searchWord() {
+        const searchWord = document.getElementById('word-input').value.trim();
+  if (searchWord) {
+    if (dictionary.includes(searchWord)) {
+      document.getElementById('result').innerHTML = `<span class="text-success">"${searchWord}" is in the dictionary!</span>`;
+    } else {
+      document.getElementById('result').innerHTML = `<span class="text-danger">"${searchWord}" is not in the dictionary.</span>`;
+    }
+  } else {
+    document.getElementById('result').innerHTML = `<span class="text-danger">Please enter a word to search.</span>`;
+  }
+  document.getElementById('word-input').value = '';
 }
